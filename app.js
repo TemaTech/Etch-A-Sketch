263c5baa830bf623da2a74db83c4b0748d37cfc6
Artem <artem138927@gmail.com>
2022-09-04 22:31:50 +0200

    html/css is made, next will be JS

---
+++ app.js
@@ -0,0 +1,9 @@
+// Tasks:
+// 1. Make range numbers display
+// 2. Make that you can choose only one button from: Color mode, rainbow, eraser. Clear all will work like just normal button.
+// 
+// JS for painting:
+// 1. Make grid that amount of which you can control with range input.
+// 2. When you hover palette change cursor on painting icon.
+// 3. When you press on palette make it pain.
+// 4. 
